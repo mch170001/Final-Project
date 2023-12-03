5ea6cdb624faeda8ffd2d9ed0dea7978e9d1cf90
Michael Hart <mch170001@utdallas.edu>
2023-12-03 14:58:36 -0600

    Low Deliverables

--- src/project.py
+++ src/project.py
@@ -1,5 +1,61 @@
-def main():
-    return
+import pygame
+from sys import exit
 
-if __name__ == "__main__":
-    main()
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('Hart PFDA Final Project')
+clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+    
+sky_surf = pygame.image.load('graphics/Sky.png').convert_alpha()
+ground_surf = pygame.image.load('graphics/ground.png').convert_alpha()
+
+score_surf = test_font.render('My game', False, 'Black')
+score_rect = score_surf.get_rect(center = (400, 50))
+
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(bottomright = (600,300))
+
+player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80,300))
+player_gravity = 0
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+                player_gravity = -20
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                player_gravity = -20
+
+    screen.blit(sky_surf,(0,0))
+    screen.blit(ground_surf,(0,300))
+    pygame.draw.rect(screen, 'Orange', score_rect)
+    screen.blit(score_surf, score_rect)
+    
+    snail_rect.x -= 4
+    if snail_rect.right <= 0:
+        snail_rect.left = 800
+    screen.blit(snail_surf,snail_rect)
+
+    # Player
+    player_gravity += 1
+    player_rect.y += player_gravity
+    if player_rect.bottom >= 300:
+        player_rect.bottom = 300
+    screen.blit(player_surf, player_rect)
+
+    # collision
+    if snail_rect.colliderect(player_rect):
+        pygame.quit()
+        exit()
+
+    pygame.display.update()
+    clock.tick(60)
